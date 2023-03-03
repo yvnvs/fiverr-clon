02566deaf6095f496ccf38c1907c6229376c4b3d
yvnvs <yunusinne@gmail.com>
2023-03-04 02:40:28 +0300

    trusted by component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Featured from '../../components/featured/Featured'
+import TrustedBy from '../../components/trustedBy/TrustedBy'
 import "./Home.scss"
 const Home = () => {
   return (
     <div>
       <Featured></Featured>
+      <TrustedBy></TrustedBy>
     </div>
   )
 }
