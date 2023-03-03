3e16ad157bbabdda78fde1f1a17176614a7e1423
yvnvs <yunusinne@gmail.com>
2023-03-04 01:03:00 +0300

    routes

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,11 @@
-import React, { useEffect } from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState, useEffect } from 'react'
+import { Link, useLocation } from 'react-router-dom'
 import "./Navbar.scss"
 const Navbar = () => {
     const [active, setActive] = useState(false)
     const [open, setOpen] = useState(false)
+
+    const { pathname } = useLocation()
 
     const isActive = () => {
         window.scrollY > 0 ? setActive(true) : setActive(false);
@@ -24,13 +26,13 @@ const Navbar = () => {
     }
 
     return (
-        <div className={active ? "navbar active" : "navbar"}>
+        <div className={active || pathname !== "/" ? "navbar active" : "navbar"}>
             <div className="container">
                 <div className="logo">
-                    {/* <Link to="/"> */}
-                    <span className='text'>fiverr</span>
+                    <Link to="/" className='link'>
+                        <span className='text'>fiverr</span>
 
-                    {/* </Link> */}
+                    </Link>
                     <span className='dot'>.</span>
                 </div>
                 <div className="links">
@@ -41,7 +43,7 @@ const Navbar = () => {
                     {!currenUser.isSeller && <span>Become a Seller</span>}
                     {!currenUser && <button>Join</button>}
                     {currenUser && (
-                        <div className="user" onClick={()=> setOpen(!open)}>
+                        <div className="user" onClick={() => setOpen(!open)}>
                             <img
                                 src="https://images.pexels.com/photos/1115697/pexels-photo-1115697.jpeg?auto=compress&cs=tinysrgb&w=1600"
                                 alt=""
@@ -50,26 +52,50 @@ const Navbar = () => {
                             {open && <div className="options">
                                 {currenUser?.isSeller && (
                                     <>
-                                        <span>Gigs</span>
-                                        <span>Add New Gig</span>
+                                        <Link className='link' to="/mygigs">Gigs</Link>
+                                        <Link className='link' to="/add">Add New Gig</Link>
                                     </>
                                 )}
-                                <span>Orders</span>
-                                <span>Messages</span>
-                                <span>Logout</span>
+                                <Link className='link' to="/orders">Orders</Link>
+                                <Link className='link' to="/messages">Messages</Link>
+                                <Link className='link' to="/">Logout</Link>
                             </div>}
                         </div>
                     )}
                 </div>
             </div>
 
-            {active &&
+            {(active || pathname !== "/") &&
                 (<> <hr />
                     <div className="menu">
-                        <span>Test</span>
-                        <span>Test1</span>
-                        <span>Test2</span>
-                        <span>Test3</span>
+                        <Link className="link menuLink" to="/">
+                            Graphics & Design
+                        </Link>
+                        <Link className="link menuLink" to="/">
+                            Video & Animation
+                        </Link>
+                        <Link className="link menuLink" to="/">
+                            Writing & Translation
+                        </Link>
+                        <Link className="link menuLink" to="/">
+                            AI Services
+                        </Link>
+                        <Link className="link menuLink" to="/">
+                            Digital Marketing
+                        </Link>
+                        <Link className="link menuLink" to="/">
+                            Music & Audio
+                        </Link>
+                        <Link className="link menuLink" to="/">
+                            Programming & Tech
+                        </Link>
+                        <Link className="link menuLink" to="/">
+                            Business
+                        </Link>
+                        <Link className="link menuLink" to="/">
+                            Lifestyle
+                        </Link>
+
                     </div></>)}
         </div>
     )
