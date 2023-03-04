f57959aa48b29ffabb6e0e9109524bb271fd7d02
yvnvs <yunusinne@gmail.com>
2023-03-04 04:54:00 +0300

    product slider

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,8 +3,9 @@ import Featured from "../../components/featured/Featured";
 import Slide from "../../components/slide/Slide";
 import TrustedBy from "../../components/trustedBy/TrustedBy";
 import CategoryCard from "../../components/categoryCard/CategoryCard";
-import { cards } from "../../data";
+import { cards,projects } from "../../data";
 import "./Home.scss";
+import ProjectCard from "../../components/projectCard/ProjectCard";
 const Home = () => {
   return (
     <div className="home">
@@ -92,6 +93,11 @@ const Home = () => {
           </div>
         </div>
       </div>
+      <Slide slidesToShow={4} arrowsScroll={4}>
+        {projects.map((card) => (
+          <ProjectCard key={card.id} item={card}></ProjectCard>
+        ))}
+      </Slide>
     </div>
   );
 };
